b77ab0b165fd34f0ceb570407cd0c91636f83a72
Dept. Biochem. Systems Biomed, Juntendo Univ. Grad. Sch. Med <98086219+Wada-Juntendo@users.noreply.github.com>
2024-05-06 19:23:26 +0900

    Add files via upload

--- RobustBioImageEnhancer.py
+++ RobustBioImageEnhancer.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Oct 11 19:57:59 2023
-@author: Yuichi Wada Ph.D. 
-The Department of Biochemistry and Systems Biomedicine 
-at Juntendo University Graduate School of Medicine
+Author: Yuichi Wada Ph.D. 
+The Department of Biochemistry II Juntendo University Graduate School of Medicine
 
 RobustBioImageEnhancer.py
-Copyright (C) 2023 Yuichi Wada
+Copyright (C) 2024 Yuichi Wada
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -36,14 +34,15 @@ EXCLUDE_INTENSITY_THRESHOLD = round(65535 * 0.95)
 DESIRED_MEDIAN = 10000
 DESIRED_WIDTH = 30000
 
-PATHNAME = "Your Pathname"
+PATHNAME =  "Your Pathname"
+Imagefiletype =".tif"
 
 # Get file list
-input_file_names = glob.glob(PATHNAME + "*.tif")
+input_file_names = glob.glob(PATHNAME + "/**/*"+Imagefiletype, recursive=True)
 
 for file_name in input_file_names:
     # Read image
-    image = io.imread(PATHNAME + file_name)
+    image = io.imread(file_name)
     
     # Compute histogram
     histogram, bin_edges = np.histogram(image, bins=256)
@@ -52,21 +51,7 @@ for file_name in input_file_names:
     min_bin_index = np.argmin(histogram[1:12])
     lower_threshold = bin_edges[min_bin_index - 1].astype(np.int16)
     
-    # Determine the peak of ROI and bin width
-    peak_roi_index = np.argmax(histogram[12:255])
-    roi_center_initial = bin_edges[peak_roi_index + 10]
-    bin_width = (peak_roi_index + 12) - min_bin_index
-    width_ratio = bin_width * 2 * 256
-    
-    # Suppress high intensity noise
-    image[image > EXCLUDE_INTENSITY_THRESHOLD] = 0
-    
-    # Compute desired standard deviation
-    desired_std = (DESIRED_WIDTH * 0.90) / (2 * stats.norm.ppf(0.95))
-    desired_std = round(desired_std, 3)
-    
-    # Compute scaling factor
-    alpha = desired_std / width_ratio
+    alpha= np.median(image[image>lower_threshold])/DESIRED_MEDIAN
     alpha = round(alpha, 3).astype(np.float16)
     
     # Scale intensities and adjust contrast
@@ -74,8 +59,7 @@ for file_name in input_file_names:
     image[image > lower_threshold] = image[image > lower_threshold] * alpha
     
     # Compute brightness adjustment factor
-    roi_center_adjusted = roi_center_initial * alpha
-    beta = DESIRED_MEDIAN - roi_center_adjusted - lower_threshold
+    beta = DESIRED_MEDIAN - np.median(image)
     beta = round(beta)
     
     # Adjust brightness and clip values
@@ -83,5 +67,5 @@ for file_name in input_file_names:
     image = np.clip(image, 0, 65535).astype(np.uint16)
     
     # Save the adjusted image
-    output_file_name = file_name.replace(".tif", "_adjusted.tif")
+    output_file_name = file_name.replace(Imagefiletype, "_adjusted.tif")
     tif.imwrite(output_file_name, image)
